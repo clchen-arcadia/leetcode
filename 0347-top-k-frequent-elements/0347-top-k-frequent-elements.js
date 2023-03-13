70e71dc911c10217fb7834cc78e2686b93f8ac2e
Christopher Chen <100498354+clchen-arcadia@users.noreply.github.com>
2023-03-13 18:03:32 -0400

    Time: 106 ms (18.15%), Space: 47.1 MB (26.09%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -1,48 +1,89 @@
-/**
- * Pseudocode:
- *    Scan over the nums array once to build the freq counter, O(N) process.
- *    Then scan over the keys of the freq counter once, keeping the k elements of highest count,
- *    this should be no slower than O(N) also.
- *    Edit, need to sort by highest count because any replacement must replace the element
- *    with the lowest count.
- */
-
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number[]}
- */
-var topKFrequent = function (nums, k) {
+var topKFrequent = function(nums, k) {
   const freqCount = buildLetterFreqCounter(nums);
+  const pq = new PriorityQueue1((a, b) => a[1] - b[1]);
 
-  const keys = Object.keys(freqCount);
-
-  const topElements = [];
-
-  // Seed first k elements as tuples, first the element, then its count
-  for (let i = 0; i < k; i++) {
-    topElements.push([keys[i], freqCount[keys[i]]]);
-  }
-
-  // Sort the tuples from highest freq count to lowest
-  topElements.sort((a, b) => b[1] - a[1]);
-
-  // Loop over the remaining elements, if the count is higher than the lowest count,
-  // then replace that element tuple and resort the list.
-  for (let i = k; i < keys.length; i++) {
-    if (freqCount[keys[i]] > topElements[topElements.length - 1][1]) {
-      topElements[topElements.length - 1] = [keys[i], freqCount[keys[i]]];
-      topElements.sort((a, b) => b[1] - a[1]);
+  for (let [num, count] of Object.entries(freqCount)) {
+    pq.enqueue([num, count]);
+    if (pq.size() > k) {
+      pq.dequeue();
     }
   }
 
-  // Clean the list by remove the tuple count, and leaving only the element
-  for (let idx in topElements) {
-    topElements[idx] = topElements[idx][0];
+  const topElements = [];
+  while (!pq.isEmpty()) {
+    topElements.unshift(pq.dequeue()[0]);
   }
 
   return topElements;
 };
+
+class PriorityQueue1 {
+  constructor(comparator) {
+    this.heap = [];
+    this.comparator = comparator || ((a, b) => a - b);
+  }
+
+  size() {
+    return this.heap.length;
+  }
+
+  isEmpty() {
+    return this.heap.length === 0;
+  }
+
+  enqueue(value) {
+    this.heap.push(value);
+    this.bubbleUp(this.heap.length - 1);
+  }
+
+  dequeue() {
+    const root = this.heap[0];
+    const last = this.heap.pop();
+    if (this.heap.length > 0) {
+      this.heap[0] = last;
+      this.sinkDown(0);
+    }
+    return root;
+  }
+
+  bubbleUp(index) {
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2);
+      if (this.comparator(this.heap[index], this.heap[parentIndex]) < 0) {
+        this.swap(index, parentIndex);
+        index = parentIndex;
+      } else {
+        break;
+      }
+    }
+  }
+
+  sinkDown(index) {
+    while (true) {
+      const leftChildIndex = index * 2 + 1;
+      const rightChildIndex = index * 2 + 2;
+      let minIndex = index;
+      if (leftChildIndex < this.heap.length && this.comparator(this.heap[leftChildIndex], this.heap[minIndex]) < 0) {
+        minIndex = leftChildIndex;
+      }
+      if (rightChildIndex < this.heap.length && this.comparator(this.heap[rightChildIndex], this.heap[minIndex]) < 0) {
+        minIndex = rightChildIndex;
+      }
+      if (minIndex !== index) {
+        this.swap(index, minIndex);
+        index = minIndex;
+      } else {
+        break;
+      }
+    }
+  }
+
+  swap(index1, index2) {
+    const temp = this.heap[index1];
+    this.heap[index1] = this.heap[index2];
+    this.heap[index2] = temp;
+  }
+}
 
 function buildLetterFreqCounter(str) {
   const letterFreqCounter = {};
@@ -58,3 +99,4 @@ function buildLetterFreqCounter(str) {
 
   return letterFreqCounter;
 }
+
