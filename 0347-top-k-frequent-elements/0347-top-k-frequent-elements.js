83c1eb3dd57fd82f269c462452349c0e2c1277ae
Christopher Chen <100498354+clchen-arcadia@users.noreply.github.com>
2023-03-13 21:14:37 -0400

    Time: 1077 ms (5.00%), Space: 54 MB (5.08%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -1,89 +1,72 @@
-var topKFrequent = function(nums, k) {
-  const freqCount = buildLetterFreqCounter(nums);
-  const pq = new PriorityQueue1((a, b) => a[1] - b[1]);
+/**
+ * Pseudocode:
+ *    Scan over the nums array once to build the freq counter, O(N) process.
+ *    Then scan over the keys of the freq counter once, keeping the k elements of highest count,
+ *    this should be no slower than O(N) also.
+ *    Edit, need to sort by highest count because any replacement must replace the element
+ *    with the lowest count.
+ */
 
-  for (let [num, count] of Object.entries(freqCount)) {
-    pq.enqueue([num, count]);
-    if (pq.size() > k) {
-      pq.dequeue();
-    }
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function (nums, k) {
+  const freqCount = buildLetterFreqCounter(nums);
+  const keys = Object.keys(freqCount);
+
+  const topElementsHeap = new MaxHeap((a, b) => a[1] - b[1]);
+
+  console.log(`freqCount is ${freqCount}`);
+
+  for (const [key, val] of Object.entries(freqCount)) {
+    console.log(`key is ${key} and value is ${val}`);
+    topElementsHeap.push([key, val]);
+    console.log("topElementsHeap is", topElementsHeap.tree);
   }
+
+  console.log(`topElementsHeap is ${topElementsHeap.tree}`);
 
   const topElements = [];
-  while (!pq.isEmpty()) {
-    topElements.unshift(pq.dequeue()[0]);
+
+  for (let i=0; i < k; i++) {
+    console.log("topElementsHeap before pop is", topElementsHeap.tree);
+
+    const topElement = topElementsHeap.pop();
+    console.log("topElementsHeap after pop is", topElementsHeap.tree);
+
+    console.log(`topElement is ${topElement}`);
+
+    topElements.push(topElement[0]);
   }
+
+  // Old implementation, keeping a sorted list of k elements and replacing the lowest count each time
+  // const topElements = [];
+  // // Seed first k elements as tuples, first the element, then its count
+  // for (let i = 0; i < k; i++) {
+  //   topElements.push([keys[i], freqCount[keys[i]]]);
+  // }
+
+  // // Sort the tuples from highest freq count to lowest
+  // topElements.sort((a, b) => b[1] - a[1]);
+
+  // // Loop over the remaining elements, if the count is higher than the lowest count,
+  // // then replace that element tuple and resort the list.
+  // for (let i = k; i < keys.length; i++) {
+  //   if (freqCount[keys[i]] > topElements[topElements.length - 1][1]) {
+  //     topElements[topElements.length - 1] = [keys[i], freqCount[keys[i]]];
+  //     topElements.sort((a, b) => b[1] - a[1]);
+  //   }
+  // }
+
+  // // Clean the list by remove the tuple count, and leaving only the element
+  // for (let idx in topElements) {
+  //   topElements[idx] = topElements[idx][0];
+  // }
 
   return topElements;
 };
-
-class PriorityQueue1 {
-  constructor(comparator) {
-    this.heap = [];
-    this.comparator = comparator || ((a, b) => a - b);
-  }
-
-  size() {
-    return this.heap.length;
-  }
-
-  isEmpty() {
-    return this.heap.length === 0;
-  }
-
-  enqueue(value) {
-    this.heap.push(value);
-    this.bubbleUp(this.heap.length - 1);
-  }
-
-  dequeue() {
-    const root = this.heap[0];
-    const last = this.heap.pop();
-    if (this.heap.length > 0) {
-      this.heap[0] = last;
-      this.sinkDown(0);
-    }
-    return root;
-  }
-
-  bubbleUp(index) {
-    while (index > 0) {
-      const parentIndex = Math.floor((index - 1) / 2);
-      if (this.comparator(this.heap[index], this.heap[parentIndex]) < 0) {
-        this.swap(index, parentIndex);
-        index = parentIndex;
-      } else {
-        break;
-      }
-    }
-  }
-
-  sinkDown(index) {
-    while (true) {
-      const leftChildIndex = index * 2 + 1;
-      const rightChildIndex = index * 2 + 2;
-      let minIndex = index;
-      if (leftChildIndex < this.heap.length && this.comparator(this.heap[leftChildIndex], this.heap[minIndex]) < 0) {
-        minIndex = leftChildIndex;
-      }
-      if (rightChildIndex < this.heap.length && this.comparator(this.heap[rightChildIndex], this.heap[minIndex]) < 0) {
-        minIndex = rightChildIndex;
-      }
-      if (minIndex !== index) {
-        this.swap(index, minIndex);
-        index = minIndex;
-      } else {
-        break;
-      }
-    }
-  }
-
-  swap(index1, index2) {
-    const temp = this.heap[index1];
-    this.heap[index1] = this.heap[index2];
-    this.heap[index2] = temp;
-  }
-}
 
 function buildLetterFreqCounter(str) {
   const letterFreqCounter = {};
@@ -100,3 +83,122 @@ function buildLetterFreqCounter(str) {
   return letterFreqCounter;
 }
 
+class MaxHeap {
+  constructor(comparator) {
+    this.tree = [];
+    this.comparator = comparator || ((a, b) => a - b);
+  }
+
+  size() {
+    return this.tree.length;
+  }
+
+  isEmpty() {
+    return this.tree.length === 0;
+  }
+
+  push(value) {
+    this.tree.push(value);
+    this._bubbleUp(this.tree.length - 1);
+  }
+
+  pop() {
+    const topVal = this.tree[0];
+    this.swap(0, this.tree.length - 1);
+    this.tree.pop();
+
+    this._siftDown(0);
+
+    return topVal;
+  }
+
+  _bubbleUp(idx) {
+    console.log("_bubbleUp called with ", idx);
+
+    let currIdx = idx;
+
+    while (true) {
+      if (currIdx === 0) {
+        break;
+      }
+
+      const parentIdx = this.getParent(currIdx);
+      console.log("parentIdx is", parentIdx);
+
+
+      if (
+        this.comparator(this.tree[currIdx], this.tree[parentIdx]) > 0
+      ) {
+        this.swap(currIdx, parentIdx);
+        currIdx = parentIdx;
+      } else {
+        break;
+      }
+    }
+  }
+
+  _siftDown(idx) {
+    console.log("_siftDown is called with ", idx);
+
+    let currIdx = idx;
+
+    while (true) {
+      const leftIdx = this.getLeftChild(currIdx);
+      const rightIdx = this.getRightChild(currIdx);
+      const leftVal = this.tree[leftIdx];
+      const rightVal = this.tree[rightIdx];
+      const currVal = this.tree[currIdx];
+
+      if (
+        leftVal === undefined
+        && rightVal === undefined
+      ) {
+        break;
+      } else if (
+        leftVal === undefined
+        || rightVal === undefined
+      ) {
+        const oneChildIdx = (leftVal === undefined ? rightIdx : leftIdx);
+        const oneChildVal = this.tree[oneChildIdx];
+
+        if (this.comparator(currVal, oneChildVal) < 0) {
+          this.swap(currIdx, oneChildIdx);
+          currIdx = oneChildIdx;
+        } else {
+          break;
+        }
+      } else {
+        const largestChildIdx = (
+          this.comparator(leftVal, rightVal) > 0
+            ? leftIdx
+            : rightIdx
+        );
+        const largestChildVal = this.tree[largestChildIdx];
+        if (
+          this.comparator(currVal, largestChildVal) < 0
+        ) {
+          this.swap(currIdx, largestChildIdx);
+          currIdx = largestChildIdx;
+        } else {
+          break;
+        }
+      }
+    }
+  }
+
+  getParent(idx) {
+    return Math.floor((idx - 1) / 2);
+  }
+
+  getLeftChild(idx) {
+    return 2 * idx + 1;
+  }
+
+  getRightChild(idx) {
+    return 2 * idx + 2;
+  }
+
+  swap(idx1, idx2) {
+    [this.tree[idx1], this.tree[idx2]] = [this.tree[idx2], this.tree[idx1]];
+  }
+}
