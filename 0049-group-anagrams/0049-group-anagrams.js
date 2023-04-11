bd1b6dfdd6dceb1cbdaad505665343ab84d41690
Christopher Chen <100498354+clchen-arcadia@users.noreply.github.com>
2023-04-11 12:08:13 -0400

    Time: 149 ms (26.19%), Space: 53.1 MB (64.98%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -1,73 +1,18 @@
 /**
- * Pseudocode:
- *    create a letter-freq counter for each word
- *    use shadow equality checker to compare letter-freq counters to group anagrams
- *    scan and clean this list to get the nested array output
- *
- * Pseudocode 2:
- *    comparing two strings sorted will be equal IFF they are anagrams
- *    leverage this to speed up the algorithm
- */
-
-/**
  * @param {string[]} strs
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
+    const anagramsHashMap = new Object();
 
-  const nestedTaggedWords = [];
-
-  for (let str of strs) {
-    const sortedString = Array.from(str).sort().join('');
-    const insertTuple = [[str], sortedString];
-
-    let insertBool = false;
-    for (let tuple of nestedTaggedWords) {
-      if (tuple[1] === insertTuple[1]) {
-        tuple[0].push(str);
-        insertBool = !insertBool;
-        break;
+    for (let str of strs) {
+      const sortedStr = str.split('').sort().join();
+      if (anagramsHashMap[sortedStr] === undefined) {
+        anagramsHashMap[sortedStr] = [str];
+      } else {
+        anagramsHashMap[sortedStr].push(str);
       }
     }
-    if (!insertBool) {
-      nestedTaggedWords.push(insertTuple);
-    }
 
-  }
-
-  const nestedWordsCleaned = [];
-
-  for (let tuple of nestedTaggedWords) {
-    nestedWordsCleaned.push(tuple[0]);
-  }
-
-  return nestedWordsCleaned;
+    return Object.values(anagramsHashMap);
 };
-
-function buildLetterFreqCounter(str) {
-  const letterFreqCounter = {};
-
-  for (let char of str) {
-    if (letterFreqCounter[char] === undefined) {
-      letterFreqCounter[char] = 1;
-    }
-    else {
-      letterFreqCounter[char]++;
-    }
-  }
-
-  return letterFreqCounter;
-}
-
-function checkShallowEquality(obj1, obj2) {
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-
-  if (keys1.length !== keys2.length) return false;
-
-  for (let key of keys1) {
-    if (obj1[key] !== obj2[key]) return false;
-  }
-
-  return true;
-}
