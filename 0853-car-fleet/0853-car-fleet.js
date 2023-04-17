77c69b0adf0b48bcef1d4ca25dce290d1e5ea175
Christopher Chen <100498354+clchen-arcadia@users.noreply.github.com>
2023-04-17 15:00:12 -0400

    Time: 264 ms (82.29%), Space: 70.1 MB (54.12%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} target
+ * @param {number[]} position
+ * @param {number[]} speed
+ * @return {number}
+ */
+var carFleet = function (target, positions, speeds) {
+  if (positions.length !== speeds.length) throw new Error("invalid positions and speeds");
+
+  const stitchedVectors = [];
+
+  for (let i = 0; i < positions.length; i++) {
+    stitchedVectors.push([positions[i], speeds[i]]);
+  }
+
+  stitchedVectors.sort((a, b) => a[0] - b[0]);
+
+  let fleetsCount = 0;
+  let timeToTarget = -Infinity;
+
+  while (stitchedVectors.length !== 0) {
+    const currVector = stitchedVectors.pop();
+
+    const tempTimeToTarget = (target - currVector[0]) / currVector[1];
+    if (tempTimeToTarget > timeToTarget) {
+      fleetsCount++;
+      timeToTarget = tempTimeToTarget;
+    }
+  }
+
+  return fleetsCount;
+};
