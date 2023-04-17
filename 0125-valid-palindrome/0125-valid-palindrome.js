e48eb8c86e1facf3d59e46698b876df84b7d77ff
Christopher Chen <100498354+clchen-arcadia@users.noreply.github.com>
2023-04-16 21:17:40 -0400

    Time: 77 ms (48.61%), Space: 49.6 MB (17.17%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,12 +3,25 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    let charsLower = Array.from(s.toLowerCase());
-
-    charsLower = charsLower.filter(char => (char.match(/[0-9a-z]/)));
-
-    cleanedString = charsLower.join('');
-    reverseCleanedString = charsLower.reverse().join('');
-
-    return cleanedString === reverseCleanedString;
+    if (s.length === 0) return true;
+    
+    let start = 0;
+    let end = s.length - 1;
+    
+    while (start < end) {
+        if (!s[start].toLowerCase().match(/[0-9a-z]/)) {
+            start++;
+        } else if(!s[end].toLowerCase().match(/[0-9a-z]/)){
+            end--;
+        } else {
+            if(s[start].toLowerCase() !== s[end].toLowerCase()) {
+                return false;
+            }
+            
+            start++;
+            end--;
+        } 
+    }
+    
+    return true;
 };
