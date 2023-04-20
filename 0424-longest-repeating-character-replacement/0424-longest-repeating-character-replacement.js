913b6691a66398ce7fac59fe0ca1f5348af17c6e
Christopher Chen <100498354+clchen-arcadia@users.noreply.github.com>
2023-04-20 12:49:20 -0400

    Time: 78 ms (70.80%), Space: 43 MB (42.80%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -1,51 +1,40 @@
 /**
- * https://leetcode.com/problems/longest-repeating-character-replacement/
- * Time O(((N + 26) * N) * (M - N)) | Space O(1)
  * @param {string} s
  * @param {number} k
  * @return {number}
  */
 var characterReplacement = function (s, k) {
-    let [left, right, longest, max] = new Array(4).fill(0);
-    const frequencyMap = new Array(26).fill(0);
+  let [left, right] = [0, 0];
+  let freqCounter = {};
+  freqCounter.longest = 0;
+  let outputLength = 0;
 
-    while (right < s.length) {
-        const count = addRightFrequency(s, right, frequencyMap);
+  while (right < s.length) {
+    const char = s[right];
 
-        longest = Math.max(longest, count);
-
-        let window = right - left + 1;
-        const canSlide = k < window - longest;
-        if (canSlide) {
-            subtractLeftFrequency(s, left, frequencyMap);
-            left++;
-        }
-
-        window = right - left + 1;
-        max = Math.max(max, window);
-
-        right++;
+    if (freqCounter[char] === undefined) {
+      freqCounter[char] = 1;
+    } else {
+      freqCounter[char]++;
     }
 
-    return max;
+    freqCounter.longest = Math.max(freqCounter.longest, freqCounter[char]);
+
+    let window = right - left + 1;
+
+    const canSlide = k < window - freqCounter.longest;
+
+    if (canSlide) {
+      const leftChar = s[left];
+      freqCounter[leftChar]--;
+
+      left++;
+      window = right - left + 1;
+    }
+
+    outputLength = Math.max(outputLength, window);
+    right++;
+  }
+
+  return outputLength;
 };
-
-const addRightFrequency = (s, right, map) => {
-    const char = s[right];
-    const index = getCode(char);
-
-    map[index]++;
-
-    return map[index];
-};
-
-const subtractLeftFrequency = (s, left, map) => {
-    const char = s[left];
-    const index = getCode(char);
-
-    map[index]--;
-
-    return map[index];
-};
-
-const getCode = (char) => char.charCodeAt(0) - 'A'.charCodeAt(0);
