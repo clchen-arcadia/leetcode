3f41cacd6a8e19e65c943cb0b44f8f8991221496
Christopher Chen <100498354+clchen-arcadia@users.noreply.github.com>
2023-04-20 12:03:57 -0400

    Time: 74 ms (82.94%), Space: 43.8 MB (34.18%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -1,63 +1,51 @@
 /**
+ * https://leetcode.com/problems/longest-repeating-character-replacement/
+ * Time O(((N + 26) * N) * (M - N)) | Space O(1)
  * @param {string} s
  * @param {number} k
  * @return {number}
  */
-var characterReplacement = function(s, k) {
-  let left = 0;
-  let best = 0;
-  const map = new Map();
+var characterReplacement = function (s, k) {
+    let [left, right, longest, max] = new Array(4).fill(0);
+    const frequencyMap = new Array(26).fill(0);
 
-  function checkMap(map, k) {
-    let maxTuple = [null, 0];
-    for (let key in map) {
-      if (map[key] > maxTuple[1]) {
-        maxTuple[0] = key;
-        maxTuple[1] = map[key];
-      }
+    while (right < s.length) {
+        const count = addRightFrequency(s, right, frequencyMap);
+
+        longest = Math.max(longest, count);
+
+        let window = right - left + 1;
+        const canSlide = k < window - longest;
+        if (canSlide) {
+            subtractLeftFrequency(s, left, frequencyMap);
+            left++;
+        }
+
+        window = right - left + 1;
+        max = Math.max(max, window);
+
+        right++;
     }
 
-    let sumForK = 0;
-    for (let key in map) {
-      if (key === maxTuple[0]) continue;
-      sumForK += map[key];
-    }
-
-    return k >= sumForK;
-  }
-  // function logMapElements(value, key, map) {
-  //   console.log(`m[${key}] = ${value}`);
-  // }
-
-  for (let right = 0; right < s.length; right++) {
-    // console.log(`for loop executing with right=${right}, `);
-    // console.log('map is...');
-    // map.forEach(logMapElements);
-
-    const char = s[right];
-    if (map[char] === undefined) map[char] = 1;
-    else map[char]++;
-
-    while (!checkMap(map, k)) {
-      // console.log(`while loop executing with map=...`);
-      // map.forEach(logMapElements);
-
-
-      const leftChar = s[left];
-      left++;
-      map[leftChar] -= 1;
-      if (map[leftChar] === 0) map.delete(leftChar);
-    }
-
-    best = Math.max(best, right - left + 1);
-  }
-
-  return best;
+    return max;
 };
 
-// console.log(
-//   characterReplacement(
-//   "AABABBA",
-//   1
-//   )
-// );
+const addRightFrequency = (s, right, map) => {
+    const char = s[right];
+    const index = getCode(char);
+
+    map[index]++;
+
+    return map[index];
+};
+
+const subtractLeftFrequency = (s, left, map) => {
+    const char = s[left];
+    const index = getCode(char);
+
+    map[index]--;
+
+    return map[index];
+};
+
+const getCode = (char) => char.charCodeAt(0) - 'A'.charCodeAt(0);
