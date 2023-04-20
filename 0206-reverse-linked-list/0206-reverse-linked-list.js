860e30e4d646d5a23c7b7f5e08a2e83157323325
Christopher Chen <100498354+clchen-arcadia@users.noreply.github.com>
2023-04-20 18:30:46 -0400

    Time: 69 ms (31.59%), Space: 44 MB (84.68%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -0,0 +1,30 @@
+/**
+ * (head) 1 --> 2 --> 3 --> null
+ * null <-- 1 <-- 2 <-- 3 (head)
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let curr = head;
+  let next = null;
+  let prev = null;
+
+  while (curr) {
+    next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+
+  return prev;
+};
