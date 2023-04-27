7040ae192edf97cb37b6212ab08ba642fa37164a
Christopher Chen <100498354+clchen-arcadia@users.noreply.github.com>
2023-04-27 02:14:48 -0400

    Time: 1639 ms (5.02%), Space: 60.8 MB (5.17%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -0,0 +1,57 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function (intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  const mergeGroups = [];
+
+  for (let i = 0; i < intervals.length - 1; i++) {
+    if (isInInterval(intervals[i], intervals[i + 1][0])) {
+      const group = [i, i + 1];
+
+      let j = i + 2;
+      while (j < intervals.length) {
+        if (
+          group.map(idx => intervals[idx]).some(
+            inter => isInInterval(inter, intervals[j][0])
+          )
+        ) {
+          group.push(j);
+          j++;
+        } else {
+          break;
+        }
+      }
+
+      mergeGroups.push(group);
+      i = j - 1;
+    }
+  }
+
+  mergeGroups.reverse();
+
+  for (let group of mergeGroups) {
+    intervals.splice(
+      group[0],
+      group.length,
+      mergeMany(...group.map(i => intervals[i]))
+    );
+  }
+
+  return intervals;
+};
+
+function isInInterval(interval, val) {
+  return val >= interval[0] && val <= interval[1];
+}
+
+function mergeMany(...intervals) {
+  return [
+    Math.min(...intervals.map(i => i[0])),
+    Math.max(...intervals.map(i => i[1]))
+  ];
+}
+
+console.log(merge([[2,3],[2,2],[3,3],[1,3],[5,7],[2,2],[4,6]]));
