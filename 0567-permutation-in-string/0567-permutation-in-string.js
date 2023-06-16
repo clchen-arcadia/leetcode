217df97033564b3b0bda51183f1fa5473be62bbb
Christopher Chen <100498354+clchen-arcadia@users.noreply.github.com>
2023-06-16 11:28:59 -0700

    Time: 150 ms (38.26%), Space: 48.9 MB (33.06%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.js
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -6,10 +6,28 @@
 var checkInclusion = function(s1, s2) {
   if (s1.length > s2.length) return false;
 
-  for (let i = 0; i <= s2.length - s1.length; i++) {
-    const substring = s2.slice(i, i + s1.length);
-    const freqCounter = getFreqCounter(substring);
-    if (checkPermFromFreqCounter(s1, freqCounter)) return true;
+  const s1FreqC = getFreqCounter(s1);
+
+  let s2SubstringFreqC = getFreqCounter(s2.slice(0, s1.length));
+
+  let window = [0, s1.length - 1];
+
+  while (window[1] < s2.length) {
+    if (checkFreqCounterEquality(s1FreqC, s2SubstringFreqC)) return true;
+
+    const leftChar = s2[window[0]];
+    const newChar = s2[window[1] + 1]; // might be undefined
+
+    s2SubstringFreqC[leftChar]--;
+    if (s2SubstringFreqC[leftChar] === 0) delete s2SubstringFreqC[leftChar];
+
+    if (s2SubstringFreqC[newChar] === undefined) {
+      s2SubstringFreqC[newChar] = 1;
+    } else {
+      s2SubstringFreqC[newChar]++;
+    }
+
+    window = [window[0] + 1, window[1] + 1];
   }
 
   return false;
@@ -39,4 +57,15 @@ function checkPermFromFreqCounter(string, freqCounter) {
   }
 
   return Object.keys(freqCounter).length === 0;
+}
+
+function checkFreqCounterEquality(freqC1, freqC2) {
+  const keys1 = Object.keys(freqC1);
+  if (keys1.length !== Object.keys(freqC2).length) return false;
+
+  for (let key of keys1) {
+    if (freqC1[key] !== freqC2[key]) return false;
+  }
+
+  return true;
 }
