e610d54f08e61b9c3efdd3bc0b9dc069926df367
Christopher Chen <100498354+clchen-arcadia@users.noreply.github.com>
2023-08-06 15:48:06 -0700

    Time: 66 ms (28.61%), Space: 18 MB (26.20%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
@@ -4,26 +4,57 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+###### RECURSIVE SOLUTION #####
+# class Solution:
+#     def generateTrees(self, n: int) -> list[Optional[TreeNode]]:
+
+#         def generate_trees_recurse(start, end):
+#             if start > end:
+#                 return [None,]
+
+#             all_trees = []
+
+#             for i in range(start, end + 1):
+#                 left_trees = generate_trees_recurse(start, i - 1)
+#                 right_trees = generate_trees_recurse(i + 1, end)
+
+#                 for l in left_trees:
+#                     for r in right_trees:
+#                         root_node = TreeNode(i)
+#                         root_node.left = l
+#                         root_node.right = r
+#                         all_trees.append(root_node)
+
+#             return all_trees
+
+#         return generate_trees_recurse(1, n)
+
+### DP APPROACH ###
 class Solution:
     def generateTrees(self, n: int) -> list[Optional[TreeNode]]:
+        if n == 0:
+            return []
 
-        def generate_trees_recurse(start, end):
-            if start > end:
-                return [None,]
+        dp = [[] for _ in range(n + 1)]
+        dp[0] = [None,]
 
-            all_trees = []
+        for num_nodes in range(1, n + 1):
+            for root_val in range(1, num_nodes + 1):
+                for left_tree in dp[root_val - 1]:
+                    for right_tree in dp[num_nodes - root_val]:
+                        new_root = TreeNode(root_val)
+                        new_root.left = left_tree
+                        new_root.right = self.clone_nodes(right_tree, root_val)
+                        dp[num_nodes].append(new_root)
 
-            for i in range(start, end + 1):
-                left_trees = generate_trees_recurse(start, i - 1)
-                right_trees = generate_trees_recurse(i + 1, end)
+        return dp[n]
 
-                for l in left_trees:
-                    for r in right_trees:
-                        root_node = TreeNode(i)
-                        root_node.left = l
-                        root_node.right = r
-                        all_trees.append(root_node)
-
-            return all_trees
-
-        return generate_trees_recurse(1, n)
+    def clone_nodes(self, node, offset=0):
+        if node:
+            new_node = TreeNode(node.val + offset)
+            new_node.left = self.clone_nodes(node.left, offset)
+            new_node.right = self.clone_nodes(node.right, offset)
+            return new_node
+        else:
+            return None
