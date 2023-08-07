ad25c1fa219e59ab448b7b75792f75d3364f47b4
Christopher Chen <100498354+clchen-arcadia@users.noreply.github.com>
2023-08-06 18:17:03 -0700

    Time: 62 ms (94.79%), Space: 17.7 MB (35.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,11 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        val_to_idx = {}
+
+        for idx, num in enumerate(nums):
+            val_to_idx[num] = idx
+
+        for idx, num in enumerate(nums):
+            comp_idx = val_to_idx.get(target - num)
+            if comp_idx is not None and idx != comp_idx:
+                return [idx, comp_idx]
