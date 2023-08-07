3952b1f4c90afef2c0a2abc4db41d55131c2811d
Christopher Chen <100498354+clchen-arcadia@users.noreply.github.com>
2023-08-07 12:46:45 -0700

    Time: 2497 ms (5.01%), Space: 23.4 MB (9.50%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,15 +1,32 @@
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        strs_tuples = [(str, ''.join(sorted(str))) for str in strs]
+        strs_tuples = [(str, self.get_freq_counter(str)) for str in strs]
 
-        anagrams_dict = {}
+        output_tuples = []
 
         for str_tuple in strs_tuples:
             og_str = str_tuple[0]
-            sorted_str = str_tuple[1]
-            if anagrams_dict.get(sorted_str) is None:
-                anagrams_dict[sorted_str] = [og_str]
-            else:
-                anagrams_dict[sorted_str].append(og_str)
+            str_freq_c = str_tuple[1]
 
-        return list(anagrams_dict.values())
+            is_insert = False
+
+            for output_t in output_tuples:
+                template_freq_c = output_t[0]
+                anagrams = output_t[1]
+                if template_freq_c == str_freq_c:
+                    anagrams.append(og_str)
+                    is_insert = True
+                    break
+
+            if is_insert is False:
+                output_tuples.append((str_freq_c, [og_str]))
+
+        return [t[1] for t in output_tuples]
+
+    def get_freq_counter(self, str):
+        freq_counter = {}
+
+        for char in str:
+            freq_counter[char] = freq_counter.get(char, 0) + 1
+
+        return freq_counter
