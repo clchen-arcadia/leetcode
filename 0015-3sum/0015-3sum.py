6b5684281b5685c4bf6d827238cc60f7fa682507
Christopher Chen <100498354+clchen-arcadia@users.noreply.github.com>
2023-08-08 13:38:53 -0700

    Time: 843 ms (95.19%), Space: 20.5 MB (40.97%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,28 +1,27 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
+        triplets = []
 
-        for i, a in enumerate(nums):
-            # Skip positive integers
-            if a > 0:
+        for i in range(len(nums) - 2):
+            if nums[i] > 0:
                 break
 
-            if i > 0 and a == nums[i - 1]:
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum < 0:
-                    l += 1
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                sum = nums[i] + nums[j] + nums[k]
+                if sum < 0:
+                    j += 1
+                elif sum > 0:
+                    k -= 1
                 else:
-                    res.append([a, nums[l], nums[r]])
-                    l += 1
-                    r -= 1
-                    while nums[l] == nums[l - 1] and l < r:
-                        l += 1
-                        
-        return res
+                    triplets.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+
+        return triplets
