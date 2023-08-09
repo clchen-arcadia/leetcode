2760af10a308802e92cd1e2016fad3ed63e3d1cd
Christopher Chen <100498354+clchen-arcadia@users.noreply.github.com>
2023-08-09 13:58:08 -0700

    Time: 39 ms (92.07%), Space: 16.7 MB (63.07%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,27 +1,28 @@
 class Solution:
     def generateParenthesis(self, n: int) -> list[str]:
-
+        stack = []
         output = []
 
-        def helper_recurse(opened, closed, s):
+        def helper_recurse(t):
+            (opened, closed) = t
             if opened == 0 and closed == 0:
-                output.append(s)
+                output.append(''.join(stack))
                 return
             if opened:
-                s += '('
+                stack.append('(')
                 opened -= 1
                 closed += 1
-                helper_recurse(opened, closed, s)
-                s = s[:-1]
+                helper_recurse((opened, closed))
+                stack.pop()
                 opened += 1
                 closed -= 1
             if closed:
-                s += ')'
+                stack.append(')')
                 closed -= 1
-                helper_recurse(opened, closed, s)
-                s = s[:-1]
+                helper_recurse((opened, closed))
+                stack.pop()
                 closed += 1
 
-        helper_recurse(n, 0, '')
+        helper_recurse((n, 0))
 
         return output
