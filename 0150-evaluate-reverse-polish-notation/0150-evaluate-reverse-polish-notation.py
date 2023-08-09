db4679bc49c6d1dc44a5e310181ca0304f7a77d1
Christopher Chen <100498354+clchen-arcadia@users.noreply.github.com>
2023-08-08 21:40:11 -0700

    Time: 71 ms (92.41%), Space: 16.8 MB (18.37%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,38 +1,20 @@
 class Solution:
     def evalRPN(self, tokens: list[str]) -> int:
-        func_map = {
-            '+' : self.add,
-            '-' : self.subtract,
-            '*' : self.multiply,
-            '/' : self.divide,
-        }
+        stack = []
 
-        def eval_recurse(key):
-            func = func_map[key]
-            operand2 = tokens.pop()
-            if operand2 in func_map:
-                operand2 = eval_recurse(operand2)
-            operand1 = tokens.pop()
-            if operand1 in func_map:
-                operand1 = eval_recurse(operand1)
-            return func(int(operand1), int(operand2))
+        for token in tokens:
+            match token:
+                case '+':
+                    stack.append(stack.pop() + stack.pop())
+                case '-':
+                    op2, op1 = stack.pop(), stack.pop()
+                    stack.append(op1 - op2)
+                case '*':
+                    stack.append(stack.pop() * stack.pop())
+                case '/':
+                    op2, op1 = stack.pop(), stack.pop()
+                    stack.append(int(op1 / op2))
+                case _:
+                    stack.append(int(token))
 
-        output = tokens.pop()
-        if output in func_map:
-            output = eval_recurse(output)
-        else:
-            output = int(output)
-
-        return output
-
-    def add(self, x: int, y: int) -> int:
-        return x + y
-
-    def subtract(self, x: int, y: int) -> int:
-        return x - y
-
-    def multiply(self, x: int, y: int) -> int:
-        return x * y
-
-    def divide(self, x: int, y: int) -> int:
-        return int(x / y)
+        return stack[0]
