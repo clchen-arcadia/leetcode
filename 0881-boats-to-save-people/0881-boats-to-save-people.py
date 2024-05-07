4e1f49739b0929663ca7924d6cec95c5a2d1c422
Christopher Chen <100498354+clchen-arcadia@users.noreply.github.com>
2024-05-07 01:21:07 -0400

    Time: 354 ms (38.60%), Space: 23.4 MB (25.35%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -1,29 +1,29 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        people_sorted = sorted(people)
+        max_person = max(people)
+        buckets_people = [0] * (max_person + 1)
+
+        for weight in people:
+            buckets_people[weight] += 1
 
         count = 0
 
-        left, right = 0, len(people_sorted) - 1
+        left, right = 0, len(buckets_people) - 1
 
         while left <= right:
-            if left == right:
-                count += 1
-                left += 1
-                right -= 1
-                continue
-            if (
-                people_sorted[right] == limit
-                or people_sorted[right] + people_sorted[left] > limit
-            ):
-                count += 1
-                right -= 1
-                continue
-            if people_sorted[right] + people_sorted[left] <= limit:
-                count += 1
-                right -= 1
+            if buckets_people[left] <= 0:
                 left += 1
                 continue
-            raise Exception("should not reach this line")
+            if buckets_people[right] <= 0:
+                right -= 1
+                continue
+
+            if right + left > limit:
+                buckets_people[right] -= 1
+                count += 1
+            else:
+                buckets_people[right] -= 1
+                buckets_people[left] -= 1
+                count += 1
 
         return count
